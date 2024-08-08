e17bd6c45869741520ea61c986c2ea6def403c5a
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2024-08-08 15:37:16 +0100

    fix bootstrap directory case when no bins. Jus tput in given outdir, rather than bin directory in outdir

--- core/BootStrapper.cpp
+++ core/BootStrapper.cpp
@@ -56,9 +56,12 @@ namespace HS{
 	TString newFileName=tree->GetDirectory()->GetName();
 	TDirectory* saveDir=gDirectory;
 	TString filename = TString(gSystem->DirName(newFileName))+Form("/Boot%d.root",iboot);
-	//TString filename = fOutDir+Form("Boot%d.root",iboot);
+	if(filename.Contains(fOutDir)==kFALSE){
+	  filename = fOutDir+Form("Boot%d.root",iboot);
+	}
+	
 	//create file to save tree to first (so can be memory resident)
-	std::cout<<"BootStrapper::DivideData() create file "<< filename<<std::endl;
+	std::cout<<"BootStrapper::DivideData() create file "<< filename<<" "<<filename.Contains(fOutDir)<<std::endl;
 	auto bootFile=std::unique_ptr<TFile>{TFile::Open(filename,"recreate")};
 	//create a new tree bootstrapped to the original
 	auto bt = BootStrapTree(tree);
